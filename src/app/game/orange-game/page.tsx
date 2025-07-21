a421a27a642279f8e179563c0e3aa74119137253
1000hyehyang <ducogus12@gmail.com>
2025-07-22 04:37:48 +0900

    feat: 게임 페이지 타이틀 추가

--- src/app/game/orange-game/page.tsx
+++ src/app/game/orange-game/page.tsx
@@ -1,5 +1,9 @@
 import { TangerineGame } from "@/components/game/TangerineGame";
 
+export const metadata = {
+  title: "천혜향 게임",
+};
+
 export default function OrangeGamePage() {
   return <TangerineGame />;
 } 
