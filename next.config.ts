b813c7b820f48635322be45764f6c6f41ff35928
1000hyehyang <ducogus12@gmail.com>
2025-10-14 18:09:23 +0900

    fix: Vercel 리디렉션 설정을 next.config.ts로 이동

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,23 @@ const nextConfig: NextConfig = {
     esmExternals: true,
   },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  
+  // non-www → www 리디렉션
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '1000hyehyang.me',
+          },
+        ],
+        destination: 'https://www.1000hyehyang.me/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
