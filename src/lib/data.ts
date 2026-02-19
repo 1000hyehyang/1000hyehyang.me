ab797ba6737e4b615e72d441aaa086a719179387
1000hyehyang <ducogus12@gmail.com>
2026-02-20 03:05:15 +0900

    feat: 블로그 글 추가

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,2 +1,2 @@
 // 블로그 카테고리 목록
-export const BLOG_CATEGORIES = ["Frontend", "Backend", "DevOps", "Design", "etc"];
+export const BLOG_CATEGORIES = ["Frontend", "Backend", "DevOps", "Design", "Dev", "Project", "etc"];
