a6deca6c78e2651cb0d01665bc352a83031e21d2
1000hyehyang <ducogus12@gmail.com>
2026-06-05 23:08:14 +0900

    fix: 필요없는 홈 섹션 제거

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -7,7 +7,6 @@ import { ProfileSection } from "./ProfileSection";
 import { IntroSection } from "./IntroSection";
 import { ContactSection } from "./ContactSection";
 import { VisitorCounter } from "./VisitorCounter";
-import { DevelopmentPrinciplesSection } from "./DevelopmentPrinciplesSection";
 import { TechStackSection } from "./TechStackSection";
 
 export const HomeContent = () => {
@@ -26,7 +25,6 @@ export const HomeContent = () => {
         <VisitorCounter />
         <ProfileSection />
         <IntroSection />
-        <DevelopmentPrinciplesSection />
         <TechStackSection />
         <ContactSection />
       </main>
